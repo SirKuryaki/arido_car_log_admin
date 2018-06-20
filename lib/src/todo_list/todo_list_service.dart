d8883d5f8067642721258a4a2e5779873f0192ad
Ariel <ariel@aridocode.com>
2018-06-19 21:24:50 -0300

    - added firebase login

--- lib/src/todo_list/todo_list_service.dart
+++ lib/src/todo_list/todo_list_service.dart
@@ -1,11 +1,48 @@
 import 'dart:async';
 
+import 'package:firebase/firebase.dart' as fb;
+
 import 'package:angular/core.dart';
 
-/// Mock service emulating access to a to-do list stored on a server.
 @Injectable()
 class TodoListService {
   List<String> mockTodoList = <String>[];
 
   Future<List<String>> getTodoList() async => mockTodoList;
+
+  fb.User user;
+
+  fb.Auth _fbAuth;
+  fb.GoogleAuthProvider _fbGoogleAuthProvider;
+
+  TodoListService(){
+    fb.initializeApp(
+          apiKey:"XXX",
+          authDomain: "XXX",
+          databaseURL: "XXX",
+          projectId: "XXX",
+          storageBucket: "XXX",
+          messagingSenderId: "XXX");
+
+    _fbGoogleAuthProvider = new fb.GoogleAuthProvider();
+    _fbAuth = fb.auth();
+    _fbAuth.onAuthStateChanged.listen(_authChanged);
+  }
+
+  void _authChanged(fb.User fbUser) {
+    user = fbUser;
+  }
+
+  Future signIn() async {
+    try {
+      await _fbAuth.signInWithPopup(_fbGoogleAuthProvider);
+    }
+    catch (error) {
+      print("$runtimeType::login() -- $error");
+    }
+  }
+
+  void signOut() {
+    _fbAuth.signOut();
+  }
 }
